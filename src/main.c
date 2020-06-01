682ae6d109fbe069a2af8042de9cf63a34e8f48b
Ramiro Garay <ramiro.sgaray@gmail.com>
2020-06-01 10:29:44 -0300

    Version preliminar de main: parsea e imprime AST

--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
+#include "AST.h"
+#include "history.h"
+#include "parser.h"
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hola mundo!");
+  char *line = NULL;
+
+  while (line = readline("sosh> ")) {
+    // Si no es una linea vacia
+    history_add(line);
+    read_from(line);
+    size_t line_len = strlen(line);
+    ASTNode *root = parse();
+    if (root) {
+      printf("==============\n");
+      print_node(root, 0);
+      printf("==============\n");
+      free_node(root);
+    }
+  }
+
+  history_delete();
+  printf("\n");
   return 0;
 }
